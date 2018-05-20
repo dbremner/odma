0b7073a767ca1984d55bb00a24d53d1566a24046
David Bremner <dbremner@gmail.com>
2018-05-20 14:15:54 -0700

    revert use of CString in GetAppDefaultDmsId

--- 32-bit/Connection Manager/registry.cpp
+++ 32-bit/Connection Manager/registry.cpp
@@ -18,7 +18,6 @@
 #include <string.h>
 #include <limits.h>
 #include <assert.h>
-#include <atlstr.h>
 #include "conman.h"
 
 	#define ODMA_KEY	"ODMA32"
@@ -106,20 +105,24 @@ int ODMRegistry::GetAppDefaultDmsId( LPCSTR lpszAppId, LPSTR lpszDmsId )
 		return 0;
 
 
-	CString dmsKey{ lpszAppId };
-	dmsKey += "\\";
-	dmsKey += ODMA_KEY;
+	auto * DMSKey = new char[ODM_APPID_MAX + 1 + sizeof(ODMA_KEY)];
+	strncpy(DMSKey, lpszAppId, ODM_APPID_MAX);
+	DMSKey[ODM_APPID_MAX - 1 ] = '\0';     // ensure NULL termination
+	strcat(DMSKey, "\\" );
+	strcat(DMSKey, ODMA_KEY);
 	
 	LONG nLen = ODM_DMSID_MAX;
 	char *DMSName = new char[nLen];
 
-	if (RegQueryValue(HKEY_CLASSES_ROOT, dmsKey, DMSName, &nLen) != ERROR_SUCCESS )
+	if (RegQueryValue(HKEY_CLASSES_ROOT, DMSKey, DMSName, &nLen) != ERROR_SUCCESS )
 	{
+		delete[] DMSKey;
 		delete[] DMSName;
 		return -1;
 	}
 
 	strcpy_s(lpszDmsId, ODM_DMSID_MAX, DMSName);
+	delete[] DMSKey;
 	delete[] DMSName;
 	
 	return 0;
